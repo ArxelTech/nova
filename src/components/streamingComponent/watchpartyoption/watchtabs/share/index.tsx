807395274754d177d2ac8793f63db045a185ab3e
egileekada <egileoniso.ekada@gmail.com>
2023-08-30 18:41:11 +0100

    update on friend tab

--- src/components/streamingComponent/watchpartyoption/watchtabs/share/index.tsx
+++ src/components/streamingComponent/watchpartyoption/watchtabs/share/index.tsx
@@ -12,12 +12,12 @@ function ShareTab(props: Props) {
         <Box width="full" bg="brand.secondarybg" display={"flex"} flexDirection={"column"} mt="20px" border="1px" rounded="10px" borderColor="#FFFFFF0D" color="white" py="15px" >
             <Flex alignItems={"center"} px={"10px"} borderBottom={"1px"} borderColor={"#FFFFFF0D"} justifyContent={"space-between"} >
                 <Flex gap={"8px"} >
-                    <Text as={"button"} onClick={()=> setTab(false)} fontFamily={"500"} fontSize={"sm"} pb={"3px"} borderBottom={"1px"} px={"10px"} mb={"-1px"} color={!tab ? "brand.base" : "brand.inactive"} borderColor={!tab ? "brand.base": "#FFFFFF0D"}>Link</Text>
-                    <Text as={"button"} onClick={()=> setTab(true)}  fontFamily={"500"} fontSize={"sm"} pb={"3px"} borderBottom={"1px"} px={"10px"} mb={"-1px"}  color={tab ? "brand.base" : "brand.inactive"} borderColor={tab ? "brand.base": "#FFFFFF0D"} >Room ID</Text>
+                    <Text as={"button"} onClick={()=> setTab(false)} fontFamily={"500"} fontSize={"sm"} pb={"3px"} borderBottom={"1px"} px={"10px"} mb={"-1px"} color={!tab ? "brand.primaryColor" : "brand.inactive"} borderColor={!tab ? "brand.primaryColor": "#FFFFFF0D"}>Link</Text>
+                    <Text as={"button"} onClick={()=> setTab(true)}  fontFamily={"500"} fontSize={"sm"} pb={"3px"} borderBottom={"1px"} px={"10px"} mb={"-1px"}  color={tab ? "brand.primaryColor" : "brand.inactive"} borderColor={tab ? "brand.primaryColor": "#FFFFFF0D"} >Room ID</Text>
                 </Flex>
             </Flex>
             <Box pt={"19px"} px={"22px"}  >
-                <Text fontFamily={"500"} color={"brand.base"} fontSize={"sm"} >https://www.getnova.app/in/902829</Text>
+                <Text fontFamily={"500"} color={"brand.primaryColor"} fontSize={"sm"} >https://www.getnova.app/in/902829</Text>
             
                 <Button height={"36px"} px="20px" mt={"10px"} w="fit-content" rounded="8px" _hover={{ backgroundColor: "transparent"}} _focus={{ backgroundColor: "transparent"}} border="1px" bg="transparent" fontSize={"14px"} color="white" variant='outline' >
                     Copy Link
--- src/pages/streaming/index.tsx
+++ src/pages/streaming/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from '@chakra-ui/react' 
+import { Box, Flex, Grid, Text } from '@chakra-ui/react' 
 import Topbar from '../../components/topbar'
 import { colorPallette } from '../../theme/theme' 
 import StreamingTab from '../../components/streamingComponent/streamingTab'
@@ -9,19 +9,23 @@ function Streaming() {
     // const {} = props
 
     return (
-        <Flex w="full" h="100vh" overflowY={"hidden"} bg={colorPallette.black} >
-            <Box width="full" h="100vh" borderRight="1px">
-                <Topbar user={false} />
-                <Box width="full" h={"full"} flexWrap={"inherit"} flex={1} display={"flex"} bg="blue.300" >
-                    <Text mt="auto" >Hello</Text>
+        <Grid h="100vh" w={"full"}  overflowY={"hidden"} >
+            <Flex w="full" h="full" overflowY={"hidden"} bg={colorPallette.black} >
+                <Box position={"relative"} width="full" h="full" borderRight="1px"> 
+                    <Box width="full" position={"absolute"} zIndex={"30"} top={"0px"} >
+                        <Topbar user={false} />
+                    </Box>
+                    <Box width="full" h={"100vh"} pt={"130px"} flexWrap={"inherit"} flex={1} display={"flex"} bg="blue.300" >
+                        <Text mt="auto" >Hello</Text>
+                    </Box>
                 </Box>
-            </Box>
-            <Box w="fit-content" >
-                <Box width="390px" position={"relative"} px="28px" h={"100vh"} overflowY={"auto"} >
-                    <StreamingTab />
+                <Box w="fit-content" >
+                    <Box width="460px" position={"relative"} px="28px" h={"100vh"} overflowY={"auto"} >
+                        <StreamingTab />
+                    </Box>
                 </Box>
-            </Box>
-        </Flex>
+            </Flex>
+        </Grid>
     )
 }
 
